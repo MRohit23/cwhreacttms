f79f5b7fba0c19948a0e9ab6a239b5d5bac8c0d9
Rohit_R_Magar <magarrohit1953@gmail.com>
2024-02-29 04:21:22 +0530

    final

---
+++ src/mycomponents/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div>
+            This is an About component
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
+                consequuntur sed soluta. Qui aspernatur debitis, eos mollitia
+                reprehenderit eaque exercitationem expedita quisquam, nobis
+                omnis quos earum. Iusto voluptas quia totam eum modi quasi
+                voluptates!
+            </p>
+        </div>
+    );
+};
